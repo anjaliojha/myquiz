8bc163f59b2989bea99acec5b36bd88322f1fa47
Elizabeth Joseph <74094471+Elizabeth-Joseph@users.noreply.github.com>
2021-04-19 11:57:32 +0530

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,70 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+    question.hide();
+
+    //write code to change the background color here
+    background("yellow")
+
+    //write code to show a heading for showing the result of Quiz
+    textSize(30);
+    text("RESULT OF THE QUIZ",250,80);
+
+    //call getContestantInfo( ) here
+    Contestant.getContestantInfo();
+
+    //write condition to check if contestantInfor is not undefined
+    if(allContestants!==undefined){
+      var display_answers=230;
+      fill("blue");
+      textSize(20);
+      text("*NOTE: Contestants who answered correct are highligthed in green colour|",130,230);
+
+    }
+    //write code to add a note here
+    for(var plr in allContestants) {
+      var correctAns=2;
+      if(correctAns===allContestants[plr].answer){
+        fill("red");
+        
+      }else{
+        fill("green");
+        display_answers+=30;
+        textSize(20);
+        text(allContestants[plr].name+": "+allContestants[plr].answer+": ",250,display_answers)
+
+    //write code to highlight contest who answered correctly
+    
+  }
+    }
+  }
+}
